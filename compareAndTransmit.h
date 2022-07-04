83d37dfcafd763bd66d788b3a17bdea3dabb9009
chaeyoonl <48426518+chaeyoonl@users.noreply.github.com>
2022-07-04 13:44:01 +0900

    Create compareAndTransmit.h

---
+++ compareAndTransmit.h
@@ -0,0 +1,28 @@
+
+
+#ifndef _RB_H_
+#define _RB_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+// buffer type 
+typedef struct{
+
+} Calculate;
+
+bool compare(unsigned char asc, int compareTempData);
+const char * returnTransmitData(int select);
+const char * returnTransmitData_Test();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
